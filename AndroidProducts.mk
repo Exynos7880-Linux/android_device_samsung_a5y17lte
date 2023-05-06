6eba16d57004baec1cbf3752c7de9bd831e61c32
aymanrgab <aymanrar2c@gmail.com>
2023-05-06 21:08:20 +0300

    a5y17lte: Bringup LMODroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a5y17lte.mk
+    $(LOCAL_DIR)/lmodroid_a5y17lte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lmodroid_a5y17lte-user \
+    lmodroid_a5y17lte-userdebug \
+    lmodroid_a5y17lte-eng
--- lmodroid_a5y17lte.mk
+++ lmodroid_a5y17lte.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common LMODroid stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 
 # Boot animation
